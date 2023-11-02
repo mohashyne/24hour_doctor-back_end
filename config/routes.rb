a68c3a0b36d050d325390f25037969c9461f12ce
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-11-02 14:32:31 +0300

    Install and Configure Devise and Devise Token Auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
