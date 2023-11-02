684baca0ad5795f9629e5e2489908032efd61a1b
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-11-02 14:57:59 +0300

    Generate sessions controller and create api

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  # Routes for Devise Token Auth
   mount_devise_token_auth_for 'User', at: 'auth'
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # API routes #apiendpoint /api/auth/login
+  namespace :api, defaults: { format: :json } do
+    namespace :auth do
+      post 'login', to: 'sessions#create'  # Handle login (POST request)
+    end
+  end
 end
+
