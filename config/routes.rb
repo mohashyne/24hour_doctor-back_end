0243b541fd48e51d78fcfa264e633c070a59cd2e
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-11-03 15:09:41 +0300

    Create a registration api that handles user registration, handling data validation and storage.

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
     #API endpoint for deleting a doctor already implemented
     resources :doctors
     resources :reservations, only: %i[create show]
+    #use Registration Endpoint
+    resources :users, only: [:create]
   end
 end
