413f7620c163ea345bd4e7ccb447babe6d323bb7
MSA <crystalbluetech@gmail.com>
2023-11-03 01:20:51 +0100

    configure rack-cors to control Cross-Origin Resource Sharing

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,17 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
+# config/initializers/cors.rb
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins '*' # This allows requests from any origin (http or https, guys not we can change it to strict mode if we want e.g only https request will be accepted)
+  
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: false
+    end
+  end
+  
