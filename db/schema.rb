245afbdefcf15e340b88dd171d7a9583667d59de
MSA <crystalbluetech@gmail.com>
2023-11-08 06:31:59 +0100

    add image and image_url column to doctors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_02_151827) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_08_044901) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_02_151827) do
     t.string "specialty"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image", limit: 255
+    t.string "image_url"
   end
 
   create_table "reservations", force: :cascade do |t|
